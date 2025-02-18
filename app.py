48cc389f5acee442e14424d7c43086aafe96ea0d
Antoine Hocquet <75793090+AntoineHocquet@users.noreply.github.com>
2025-02-18 19:45:40 +0000

    change code

--- app.py
+++ app.py
@@ -6,14 +6,21 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
+# Load data
 def load_data():
+  """Load data from real housing dataset""""
     url = "USA_housing_dataset.csv"  # file inside GitHub repo
     df = pd.read_csv(url)
     df['size']=df['sqft_living'] + df['sqft_lot'] + df['sqft_above'] + df['sqft_basement']
     return df
 
-def train_model():
-  df = load_data()
+def preprocess_data(df):
+    """Select relevant columns and clean data"""
+    df = df[['size', 'price']].dropna()  # Keep only size & price columns
+    return df
+
+def train_model(df):
+  """Train a linear regression model"""
   X=df[['size']]
   y=df['price']
   X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
@@ -26,24 +33,28 @@ def train_model():
 def main():
   st.title('House Price Prediction App')
 
-  st.write("Put in your house size to predict its price")
+  st.write("Put in your house size to predict its price (based on USA housing dataset)")
 
-  model = train_model()
+  #load and preprocess data
+  df=load_data()
+  df=preprocess_data(df)
+  model = train_model(df)
 
-  size = st.number_input('House size', min_value=500, max_value=2000, value=1500)
+  size = st.number_input('House size', min_value=500, max_value=5000, value=1500)
 
   if st.button('Predict price'):
     predicted_price = model.predict([[size]])
     st.success(f'Predicted price: ${predicted_price[0]:.2f}')
 
-    df = load_data()
+
     fig = px.scatter(df,x='size',y='price',title='Size vs house price')
-    fig.add_scatter(x=[size],
-                    y=[predicted_price[0]],
-                    mode='markers',
-                    marker=dict(color='red',size=15),
-                    name='Predicted Price'
-      )
+    fig.add_scatter(
+      x=[size],
+      y=[predicted_price[0]],
+      mode='markers',
+      marker=dict(color='red',size=15),
+      name='Predicted Price'
+    )
 
     st.plotly_chart(fig)
 
